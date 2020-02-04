2e7841870586bdf5c8528086ed5dad1f3546e0e9
Hongkai Liu <hongkailiu@users.noreply.github.com>
2020-02-03 22:09:41 -0500

    update python script

--- hack/duplicate_prowgen_jobs_on_build01.py
+++ hack/duplicate_prowgen_jobs_on_build01.py
@@ -29,7 +29,7 @@ with open(filename) as f:
                     build01_job["name"] += "-migrated"
                     build01_job['cluster'] = 'ci/api-build01-ci-devcluster-openshift-com:6443'
                     build01_job["labels"]["ci-operator.openshift.io/prowgen-controlled"] = "false"
-                    #build01_job["labels"]["ci-operator.openshift.io/semantics-ignored"] = "true"
+                    build01_job["labels"]["ci-operator.openshift.io/semantics-ignored"] = "true"
                     build01_job['optional'] = True
                     build01_job['skip_report'] = True
                     jobs.append(build01_job)

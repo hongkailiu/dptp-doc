1ba21278e101faa40fb8fcb6aa237cb51f42fb3c
Hongkai Liu <hongkailiu@users.noreply.github.com>
2020-04-28 15:34:04 -0400

    Add hack/generate-bw-items.sh

---
+++ hack/generate-bw-items.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+### prerequisites
+### 1. (latest version of) /usr/bin/oc, otherwise download https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/
+### 2. jq/yq are installed
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+WORKDIR="$(mktemp -d)"
+readonly WORKDIR
+
+CLUSTER_NAME="build01"
+readonly CLUSTER_NAME
+
+kubectl config use-context $CLUSTER_NAME
+
+SED_COMMAND="sed"
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  SED_COMMAND="gsed"
+fi
+
+generate_kubeconfig() {
+  local sa
+  sa=$1
+  local config
+  config="${WORKDIR}/sa.${sa}.${CLUSTER_NAME}.config"
+  oc sa create-kubeconfig -n ci "${sa}" > "${config}"
+  "${SED_COMMAND}" -i "s/${sa}/${CLUSTER_NAME}/g" "${config}"
+  for context_name in ${ADDITIONAL_CONTEXTS[@]}; do
+    new_context="$(yq '.contexts[0]' ${config} | yq -c --arg cn "${context_name}" '.name = $cn')"
+    #(hongkliu): no idea why redirection does not work
+    #yq -y --arg nc "${new_context}" '.contexts +=  [$nc | fromjson] ' "${config}" > "${config}"
+    yq -y --arg nc "${new_context}" '.contexts +=  [$nc | fromjson] ' "${config}" > "${WORKDIR}/tmp.config"
+    mv -f "${WORKDIR}/tmp.config" "${config}"
+    if [[ "$(oc --kubeconfig ${config} --context ${context_name} whoami)" != "system:serviceaccount:ci:${sa}" ]]; then
+      echo "not good kubeconfig ${config} for the expected context ${context_name}"
+      return 1
+    fi
+  done
+  if [[ "$(oc --kubeconfig ${config} --context ${CLUSTER_NAME} whoami)" != "system:serviceaccount:ci:${sa}" ]]; then
+    echo "not good kubeconfig ${config} for the expected SA ${sa}"
+    return 1
+  fi
+}
+
+declare -a SAArray=( "config-updater" "deck" "plank" "sinker" "hook" "ca-cert-issuer" "crier" )
+declare -a ADDITIONAL_CONTEXTS=( "ci/api-build01-ci-devcluster-openshift-com:6443" )
+
+# Iterate the string array using for loop
+for name in ${SAArray[@]}; do
+  if ! generate_kubeconfig "${name}"; then
+     exit 1
+  fi
+done
+
+generate_reg_auth_value() {
+  local cluster
+  cluster=$1
+  local registry
+  registry=$2
+  oc -n ci get secret $( oc get secret -n ci -o "jsonpath={.items[?(@.metadata.annotations.kubernetes\.io/service-account\.name==\"build01\")].metadata.name}" ) -o "jsonpath={.items[?(@.type==\"kubernetes.io/dockercfg\")].data.\.dockercfg}" | base64 --decode | jq --arg reg "${registry}" -r '.[$reg].auth' | tr -d '\n' > "${WORKDIR}/build01_${cluster}_reg_auth_value.txt"
+}
+
+generate_reg_auth_value build01 "image-registry.openshift-image-registry.svc:5000"
+
+echo "files are saved ${WORKDIR}"
